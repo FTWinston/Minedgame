95c0d8f6db7e79fd269cab2b344ebcfcf980ddcb
FTWinston <ftwinston@gmail.com>
2023-05-16 10:43:56 +0100

    Using vite to build generate function

---
+++ vite.generate.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/features/generator/utils/function.ts'),
+      output: {
+        entryFileNames: `generate.js`,
+      }
+    },
+    outDir: path.resolve(__dirname, 'functions'),
+  }
+})
