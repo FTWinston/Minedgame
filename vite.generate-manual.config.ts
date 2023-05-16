2fc9af99adfe310711ba119268a7d6d96a23a36e
FTWinston <ftwinston@gmail.com>
2023-05-16 20:18:51 +0100

    Separate config for manual generation

---
+++ vite.generate-manual.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/features/generator/utils/manual.ts'),
+      output: {
+        entryFileNames: `generate.js`,
+      }
+    },
+    outDir: path.resolve(__dirname, 'functions'),
+    ssr: true,
+  }
+})
