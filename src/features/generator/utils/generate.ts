458b80a5cc814d053cfbccda1649e84633b56596
FTWinston <ftwinston@gmail.com>
2023-05-16 10:44:28 +0100

    Revert "Use process.env environment variables in generate script"
    This reverts commit 25e0ef1d14eb49f8234eda4cdde39baafefb5389.

--- src/features/generator/utils/generate.ts
+++ src/features/generator/utils/generate.ts
@@ -24,15 +24,14 @@ export async function generate() {
 
 async function pushFile(definition: string) {
     const octokit = new Octokit({
+        auth: import.meta.env.VITE_GITHUB_AUTH_TOKEN
         authStrategy: createTokenAuth,
-        auth: process.env.VITE_GITHUB_AUTH_TOKEN,
     });
 
+    const owner = import.meta.env.VITE_GITHUB_OWNER;
+    const repo = import.meta.env.VITE_GITHUB_REPO;
+    const path = import.meta.env.VITE_GIT_DATA_PATH;
     await octokit.auth();
-
-    const owner = process.env.VITE_GITHUB_OWNER!;
-    const repo = process.env.VITE_GITHUB_REPO!;
-    const path = process.env.VITE_GIT_DATA_PATH!;
     
     const existingFile = await octokit.rest.repos.getContent({ owner, repo, path });
     const data = await existingFile.data;
@@ -46,10 +45,10 @@ async function pushFile(definition: string) {
         repo,
         path,
         message,
-        branch: process.env.VITE_GIT_BRANCH,
+        branch: import.meta.env.VITE_GIT_BRANCH,
         committer: {
-            name: process.env.VITE_GIT_COMMITTER_NAME!,
-            email: process.env.VITE_GIT_COMMITTER_EMAIL!,
+            name: import.meta.env.VITE_GIT_COMMITTER_NAME,
+            email: import.meta.env.VITE_GIT_COMMITTER_EMAIL,
         },
         content,
         sha,
