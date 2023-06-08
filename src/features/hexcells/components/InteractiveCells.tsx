8d7c6aabb815ef382bf479ae89dd26a4e37ed032
FTWinston <ftwinston@gmail.com>
2023-06-08 15:27:17 +0100

    Highlighted cells clear once they're all resolved

--- src/features/hexcells/components/InteractiveCells.tsx
+++ src/features/hexcells/components/InteractiveCells.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import { CellBoardInfo } from '../types/CellBoard';
-import { cellHeight } from './Cell';
+import { CellType } from '../types/CellState';
 import { isObscured } from '../utils/resolved';
 import { isClueCell } from '../utils/isClueCell';
+import { cellHeight } from './Cell';
 import { CellSet } from './CellSet';
 
 interface Props extends Omit<CellBoardInfo, 'numBombs' | 'numErrors' | 'hintsUsed'> {
@@ -27,13 +28,33 @@ const Root = styled(Box)({
     userSelect: 'none',
 });
 
+interface Focus {
+    index: number;
+    targetIndexes: number[]
+}
+
 export const InteractiveCells: React.FC<Props> = props => {
     const { columns, cells } = props;
-    const rows = Math.ceil(cells.length / columns);
 
     const [revealingIndex, setRevealingIndex] = useState<number | undefined>(undefined);
-    const [highlightIndexes, setHighlightIndexes] = useState<number[]>([]);
+    const [focus, setFocus] = useState<Focus>();
 
+    // When a cell has just been flagged or revealed,
+    // and that cell is a target of the focused cell,
+    // and no other target cells of the focused cell are obscured,
+    // then clear the focus.
+    const checkClearFocus = (index: number) => {
+        if (focus
+            && focus.targetIndexes.includes(index)
+            && focus.targetIndexes.every(
+                    targetIndex => targetIndex === index
+                || cells[targetIndex]?.type !== CellType.Obscured)
+        ) {
+                setFocus(undefined);
+        }
+    }
+
+    const rows = Math.ceil(cells.length / columns);
     const cellSizeLimitByWidth = `calc(100vw / ${columns * 1.94})`;
     const cellSizeLimitByHeight = `calc((100svh - 5rem) / ${rows - 0.25} / ${cellHeight + gapSize})`;
     const containerStyle: React.CSSProperties = {
@@ -47,21 +68,30 @@ export const InteractiveCells: React.FC<Props> = props => {
                 cells={cells}
                 style={containerStyle}
                 errorIndex={props.errorIndex}
-                highlightIndexes={highlightIndexes}
+                highlightIndexes={focus?.targetIndexes}
                 revealingIndex={revealingIndex}
                 onClick={(cell, index) => {
                     if (isObscured(cell) && !props.result) {
                         setRevealingIndex(index);
                         props.revealCell(index);
+                        checkClearFocus(index);
                     }
                     if (isClueCell(cell)) {
-                        setHighlightIndexes(highlightIndexes === cell.targetIndexes ? [] : cell.targetIndexes);
+                        setFocus(
+                            focus?.index === index
+                                ? undefined
+                                : {
+                                    index,
+                                    targetIndexes: cell.targetIndexes,
+                                }
+                        );
                     }
                 }}
                 onLongPress={(cell, index) => {
                     if (isObscured(cell) && !props.result) {
                         setRevealingIndex(index);
                         props.flagCell(index);
+                        checkClearFocus(index);
                     }
                 }}
             />
